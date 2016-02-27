85fb7e135bd4ed38173d18e61e9b7720c47dabdb
Daniela Opitz <dan.opitz@gmail.com>
2016-02-28 05:08:30 +1100

    T2. py solved

--- README.txt
+++ README.txt
@@ -2,4 +2,9 @@ Foris Challenge
 
 Language: Python 
 
+<<<<<<< Updated upstream
 T1.py -> Test 1
+=======
+T1.py -> Test 1
+T2.py -> Test 2
+>>>>>>> Stashed changes
---
+++ T2.py
@@ -0,0 +1,50 @@
+#Programa 2. Retorna el primer numero de Fibonachi que tiene mas de 1000 divisores
+#Es un tanto lento, hay que mejorar la funcion divisorGenerator
+
+
+import numpy as np
+import math
+
+#Funcion Fibonacci
+
+def fib(n, computed = {0: 0, 1: 1}):
+    if n not in computed:
+        computed[n] = fib(n-1, computed) + fib(n-2, computed)
+    return computed[n]
+
+#Funcion Divisores
+
+def divisorGenerator(n):
+    large_divisors = []
+    for i in xrange(1, int(math.sqrt(n) + 1)):
+        if n % i == 0:
+            yield i
+            if i*i != n:
+                large_divisors.append(n / i)
+    for divisor in reversed(large_divisors):
+        yield divisor
+
+N=np.zeros(100)  #Serie Fibonacci
+ND=np.zeros(100) #Numero de divisores
+
+#Generando un arreglo con los numeros de Fibonacci
+
+for i in range (len(N)):
+    N[i]=fib(i+1)
+#print N
+
+#Buscando el termino con numero de > 1000
+
+for i in range (100):
+	ND[i]=len(list(divisorGenerator(N[i])))
+	if ND[i] > 1000: 
+		print "n= " + str(i+1) + "\n"
+		print "Numero Fibonacci= " + str(fib(i+1)) + "\n"
+		break
+
+#Respuesta 498454011879264 con 1536.0 divisores
+
+
+
+
+
